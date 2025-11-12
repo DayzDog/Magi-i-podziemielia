ded4f3b1835e1219b7e23247cf65d90748a93632
zombaba22 <antiporuk12@gmail.com>
2025-11-12 17:10:23 +0100

    не получилось, нужно попробоать другой подход

--- Assets/Scripts/BoardCell.cs
+++ Assets/Scripts/BoardCell.cs
@@ -1,17 +1,17 @@
 using UnityEngine;
 
-
 public class BoardCell : MonoBehaviour
 {
-    [Tooltip("Ќеоб€зательно: логические координаты дл€ отладки/сохранений")]
+    [Tooltip("Ќеоб€зательно: логические координаты клетки дл€ отладки/сохранений")]
     public Vector2Int coord;
 
-    [Tooltip("Ћокальна€ точка прив€зки внутри клетки (0,0,0 = центр)")]
+    [Tooltip("“очка прив€зки относительно pivot клетки (0,0,0 = центр)")]
     public Vector3 snapPointLocal = Vector3.zero;
 
     [HideInInspector] public bool occupied;
     [HideInInspector] public Transform occupant;
 
+    // ћирова€ точка прив€зки (куда ставим тайл)
     public Vector3 WorldSnapPoint => transform.TransformPoint(snapPointLocal);
 
     public void SetOccupant(Transform t)
--- Assets/Scripts/TileDefinition.cs
+++ Assets/Scripts/TileDefinition.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Tiles/Tile Definition")]
 public class TileDefinition : ScriptableObject
 {
-    public string id;
-    public GameObject tilePrefab;   // модель тайла, которую ставим на поле
-    public Material previewMaterial; // полупрозрачный материал для “превью” (необязательно)
-    public Vector3 previewYOffset = new Vector3(0, 0.01f, 0); // чуть приподнять “превью”
+    [Header("Prefab реального тайла")]
+    public GameObject tilePrefab;
+
+    [Header("Как показывать 'превью'")]
+    public Material previewMaterial;         // полупрозрачный материал для ghost-модели
+    public Vector3 previewYOffset = new Vector3(0, 0.01f, 0); // приподнять превью
 }
+
